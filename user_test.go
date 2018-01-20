397ec0b92d07c0565f796509008c887239d2a402
tectiv3 <git@getpanda.me>
2018-01-20 18:59:11 +0900

    Fix formatting

--- user_test.go
+++ user_test.go
@@ -12,13 +12,13 @@ var (
 		Password: "3cb5561daa49bd5b4438ad214a6f9a6d9b056a2c0b9a91991420ad9d658b8fac",
 	}
 	register = sf.User{
-		Email:       "user2@local",
-		Password:    "3cb5561daa49bd5b4438ad214a6f9a6d9b056a2c0b9a91991420ad9d658b8fac",
-		PwCost:     101000,
-		PwSalt:     "685bdeca99977eb0a30a68284d86bbb322c3b0ee832ffe4b6b76bd10fe7b8362",
-		PwAlg:      "sha512",
+		Email:     "user2@local",
+		Password:  "3cb5561daa49bd5b4438ad214a6f9a6d9b056a2c0b9a91991420ad9d658b8fac",
+		PwCost:    101000,
+		PwSalt:    "685bdeca99977eb0a30a68284d86bbb322c3b0ee832ffe4b6b76bd10fe7b8362",
+		PwAlg:     "sha512",
 		PwKeySize: 512,
-		PwFunc:     "pbkdf2",
+		PwFunc:    "pbkdf2",
 	}
 )
 
