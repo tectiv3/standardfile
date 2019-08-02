bd5736a58dd71287185162bc7b00c91e7a92a108
David Maciel <david.maciel@kaplan.com>
2019-08-01 21:46:20 -0400

    Update Readme, Load config files from Env

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 FROM alpine:3.10 AS build-env
 RUN apk update && apk add --update go=1.12.6-r0 gcc=8.3.0-r0 g++=8.3.0-r0 git=2.22.0-r0
 WORKDIR /src
-COPY . . 
+COPY . .
 RUN go mod download
 RUN go mod verify
 RUN export CGO_ENABLED=1 && go build -o /src/bin/sf
@@ -13,7 +13,13 @@ RUN apk update && apk add --no-cache sqlite
 RUN mkdir -p /stdfile/db
 WORKDIR /app
 COPY --from=build-env /src/bin/sf /app/sf
-COPY --from=build-env /src/standardfile.json /app/standardfile.json
 VOLUME /stdfile/db
+ENV STANDARDFILE_LOAD_CONF_ENV true
+ENV DB /stdfile/db/sf.db
+ENV PORT 8888
+ENV FOREGROUND true
+ENV DEBUG false
+ENV NOREG false
+ENV CORS false
 EXPOSE 8888
 ENTRYPOINT ["/app/sf"]
--- main.go
+++ main.go
@@ -14,13 +14,14 @@ import (
 
 	"github.com/heetch/confita"
 	"github.com/heetch/confita/backend"
+	"github.com/heetch/confita/backend/env"
 	"github.com/heetch/confita/backend/file"
 	"github.com/heetch/confita/backend/flags"
 )
 
 type Config struct {
-	DB         string `config:"db"`
-	Port       int    `config:"port"`
+	DB         string `config:"db,required"`
+	Port       int    `config:"port,required"`
 	Socket     string `config:"socket"`
 	NoReg      bool   `config:"noreg"`
 	Debug      bool   `config:"debug"`
@@ -53,13 +54,33 @@ var BuildTime = ""
 func init() {
 	confBackends := []backend.Backend{}
 
-	configs := []string{"standardfile.json", "standardfile.toml", "standardfile.yaml"}
-	for _, c := range configs {
-		if _, err := os.Stat(c); err == nil {
-			confBackends = append(confBackends, file.NewBackend(c))
+	// this is a workaround for a couple of issues with the config library being
+	// used (https://github.com/heetch/confita):
+	// 1.- Confita doesn't allow setting prefixes to env vars: since the config
+	// variables are too generic (Port, Debug) there is a posibility that an existing
+	// ENV var overwrite a config file or flag. This is supposed to be fixed here:
+	// https://github.com/heetch/confita/pull/33
+	// 2.- Right now confita is not following the right order when loading values
+	// from multiple backends, the current behavior is broken with what is supposelly they explain
+	// in the docs (first match wins). There is an open issue for this:
+	// https://github.com/heetch/confita/issues/64
+	//
+	// The only two posible solutions are: wait for both issues to be fixed, or
+	// switch to a diferrent config library (https://github.com/spf13/viper)
+	//
+	// In the meantime having "STANDARDFILE_LOAD_CONF_ENV" as an  ENV var flag
+	// will maintain BC while allowing to use ENV vars inside a docker container
+	if _, ok := os.LookupEnv("STANDARDFILE_LOAD_CONF_ENV"); ok {
+		confBackends = append(confBackends, env.NewBackend())
+	} else {
+		configs := []string{"standardfile.json", "standardfile.toml", "standardfile.yaml"}
+		for _, c := range configs {
+			if _, err := os.Stat(c); err == nil {
+				confBackends = append(confBackends, file.NewBackend(c))
+			}
 		}
+		confBackends = append(confBackends, flags.NewBackend())
 	}
-	confBackends = append(confBackends, flags.NewBackend())
 	loader := confita.NewLoader(confBackends...)
 	err := loader.Load(context.Background(), &cfg)
 	if err != nil {
